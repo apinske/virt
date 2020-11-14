26648662ba737c76dca831c9cc2b4dce5d86f168
Alexander Pinske <alex@pinske.eu>
2020-11-14 11:29:52 +0000

    use local gcc

--- aarch64/boot.sh
+++ aarch64/boot.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-CC="sudo docker run --rm -it -v $PWD:/usr/src/app -w /usr/src/app gcc gcc"
-
-sudo rm hypervisor
-$CC -o hypervisor hypervisor.c
+rm hypervisor
+gcc -o hypervisor hypervisor.c
 ./hypervisor
