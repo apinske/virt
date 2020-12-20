0f65f97cc40a44a242657c7ef12bf04887002e4b
Alexander Pinske <alex@pinske.eu>
2020-12-20 21:23:55 +0100

    fix update script

--- macos/update.sh
+++ macos/update.sh
@@ -2,7 +2,7 @@
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
     scp macmini-ubuntu:~/simpleos_x86/macos/linux-5.9.13/arch/x86/boot/bzImage vmlinuz
-    scp -C macmini-ubuntu:~/simpleos_x86/macos/vda.img vmlinuz
+    scp -C macmini-ubuntu:~/simpleos_x86/macos/vda.img vda.img
 else
     scp ubuntu:~/simpleos/macos/linux-5.9.13/arch/arm64/boot/Image vmlinuz
     scp -C ubuntu:~/simpleos/macos/vda.img vda.img
