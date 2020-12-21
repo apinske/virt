23806deeb9c258c0400801d923c590383e04b1e2
Alexander Pinske <alex@pinske.eu>
2020-12-21 12:20:58 +0000

    split build script

--- macos/build.sh
+++ macos/build.sh
@@ -18,38 +18,9 @@ if [ "$ARCH" = "x86_64" ]; then
     make CC=clang LLVM=1 -j2
     cp arch/x86/boot/bzImage ../vmlinuz
     cp .config ../config-linux-x86_64
-    ARCH=x86_64
 else
     make CC=clang LLVM=1 LLVM_IAS=1 -j2
     cp arch/arm64/boot/Image ../vmlinuz
     cp .config ../config-linux-arm64
-    ARCH=aarch64
 fi
 cd ..
-
-if [ ! -f alpine-minirootfs-3.12.3-$ARCH.tar.gz ]; then
-    wget https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/$ARCH/alpine-minirootfs-3.12.3-$ARCH.tar.gz
-fi
-
-umount mnt
-dd if=/dev/zero of=vda.img bs=1M count=512
-mkfs.ext4 vda.img
-mkdir mnt
-mount vda.img mnt
-cd mnt
-tar xf ../alpine-minirootfs-3.12.3-$ARCH.tar.gz
-rm -rf etc/logrotate.d etc/modprobe.d etc/network
-rm etc/modules etc/udhcpd.conf etc/sysctl.conf etc/hostname etc/motd etc/issue etc/shadow
-cp ../etc/passwd etc/passwd
-cp ../etc/group etc/group
-cp ../etc/hosts etc/hosts
-cp ../etc/inittab etc/inittab
-cp ../etc/fstab etc/fstab
-cp -Tr ../etc/init.d etc/init.d
-mkdir etc/dropbear
-cp -Tr ../root root
-cp ../usr/share/udhcpc/default.script usr/share/udhcpc/default.script
-rm -rf lib/sysctl.d
-rm -rf media opt srv
-cd ..
-umount mnt
---
+++ macos/buildImage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ ! -f alpine-minirootfs-3.12.3-$ARCH.tar.gz ]; then
+    wget https://dl-cdn.alpinelinux.org/alpine/v3.12/releases/$ARCH/alpine-minirootfs-3.12.3-$ARCH.tar.gz
+fi
+
+umount mnt
+dd if=/dev/zero of=vda.img bs=1M count=512
+mkfs.ext4 vda.img
+mkdir mnt
+mount vda.img mnt
+cd mnt
+tar xf ../alpine-minirootfs-3.12.3-$ARCH.tar.gz
+rm -rf etc/logrotate.d etc/modprobe.d etc/network
+rm etc/modules etc/udhcpd.conf etc/sysctl.conf etc/hostname etc/motd etc/issue etc/shadow
+cp ../etc/passwd etc/passwd
+cp ../etc/group etc/group
+cp ../etc/hosts etc/hosts
+cp ../etc/inittab etc/inittab
+cp ../etc/fstab etc/fstab
+cp -Tr ../etc/init.d etc/init.d
+mkdir etc/dropbear
+cp -Tr ../root root
+cp ../usr/share/udhcpc/default.script usr/share/udhcpc/default.script
+rm -rf lib/sysctl.d
+rm -rf media opt srv
+cd ..
+umount mnt
