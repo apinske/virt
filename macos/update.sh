cef1fdbeb3e3868cd43fcb8b37639b4bec874fe9
Alexander Pinske <alex@pinske.eu>
2020-12-21 15:13:33 +0100

    cleanup

--- macos/update.sh
+++ macos/update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
-    scp macmini-ubuntu:~/simpleos_x86/macos/linux-5.9.13/arch/x86/boot/bzImage vmlinuz
+    scp macmini-ubuntu:~/simpleos_x86/macos/vmlinuz vmlinuz
     scp -C macmini-ubuntu:~/simpleos_x86/macos/vda.img vda.img
 else
-    scp ubuntu:~/simpleos/macos/linux-5.9.13/arch/arm64/boot/Image vmlinuz
+    scp ubuntu:~/simpleos/macos/vmlinuz vmlinuz
     scp -C ubuntu:~/simpleos/macos/vda.img vda.img
 fi
