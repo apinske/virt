b4eb8a823753b1683bb436c3567db1a36892810f
Alexander Pinske <alex@pinske.eu>
2021-01-28 22:05:12 +0000

    version updates

--- macos/build.sh
+++ macos/build.sh
@@ -7,11 +7,11 @@ if [ ! -d mnt ]; then
    apt install -y wget bc make clang llvm lld flex bison libelf-dev libncurses-dev libssl-dev
 fi
 
-if [ ! -d linux-5.10.1 ]; then
-    wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.1.tar.xz
-    tar xf linux-5.10.1.tar.xz
+if [ ! -d linux-5.10.11 ]; then
+    wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.11.tar.xz
+    tar xf linux-5.10.11.tar.xz
 fi
-cd linux-5.10.1
+cd linux-5.10.11
 cp ../config-linux-$ARCH .config
 if [ "$ARCH" = "x86_64" ]; then
     unset ARCH
