cebf2260fbf4984cd755ededb605307788f1a892
Alexander Pinske <alex@pinske.eu>
2021-03-11 15:35:35 +0100

    faster update script

--- macos/update.sh
+++ macos/update.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
-    scp macmini-ubuntu:~/simpleos_x86/macos/vmlinuz vmlinuz
-    scp -C macmini-ubuntu:~/simpleos_x86/macos/vda.img vda.img
+    ssh macmini-ubuntu tar czf - -C simpleos_x86/macos vda.img vmlinuz | tar xzf -
 else
-    scp ubuntu:~/simpleos/macos/vmlinuz vmlinuz
-    scp -C ubuntu:~/simpleos/macos/vda.img vda.img
+    ssh ubuntu tar czf - -C simpleos/macos vda.img vmlinuz | tar xzf -
 fi
