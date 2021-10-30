c6d5e700d038847b07102a6ae8f87539e5fb0e79
Alexander Pinske <alex@pinske.eu>
2021-10-30 19:53:10 +0200

    cleanup
    cleanup

--- macos/update.sh
+++ macos/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
-    ssh macmini-ubuntu tar czf - -C simpleos_x86/macos vda.img vmlinuz | tar xzf -
+    ssh srv2-ubuntu tar czf - -C simpleos_x86/macos vda.img vmlinuz | tar xzf -
 else
     ssh ubuntu tar czf - -C simpleos/macos vda.img vmlinuz | tar xzf -
 fi
