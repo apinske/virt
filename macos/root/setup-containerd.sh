e96953259717d920b5ce0f3041278e3fe6d124d9
Alexander Pinske <alex@pinske.eu>
2021-11-24 21:43:17 +0000

    nerdctl 0.14.0

--- macos/root/setup-containerd.sh
+++ macos/root/setup-containerd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /usr/local/bin/nerdctl ]; then
-  VERSION=0.12.1
+  VERSION=0.14.0
   ARCH=arm64
   if [ "$(uname -m)" = "x86_64" ]; then
     ARCH=amd64
@@ -17,33 +17,5 @@ mkdir /mnt/vdb/containerd
 ln -s /mnt/vdb/containerd /var/lib/containerd
 
 apk add containerd cni-plugins iptables ip6tables
-
-mkdir -p /etc/cni/net.d/
-cat >/etc/cni/net.d/10-mynet.conf <<EOF
-{
-  "cniVersion": "0.2.0",
-  "name": "mynet",
-  "type": "bridge",
-  "bridge": "cni0",
-  "isGateway": true,
-  "ipMasq": true,
-  "ipam": {
-    "type": "host-local",
-    "subnet": "10.22.0.0/16",
-    "routes": [
-      { "dst": "0.0.0.0/0" }
-    ]
-  }
-}
-EOF
-
-cat >/etc/cni/net.d/99-loopback.conf <<EOF
-{
-  "cniVersion": "0.2.0",
-  "name": "lo",
-  "type": "loopback"
-}
-EOF
-
 echo "::respawn:/usr/bin/containerd 2>&1 | logger -t containerd" >> /etc/inittab
 reboot
