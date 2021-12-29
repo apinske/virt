376d404e885167815e61a5f6382dfda5f183bb11
Alexander Pinske <alex@pinske.eu>
2021-12-29 17:53:29 +0100

    cleanup

--- macos/virt/virt/main.swift
+++ macos/virt/virt/main.swift
@@ -8,102 +8,102 @@
 import Foundation
 import Virtualization
 
-let verbose = CommandLine.arguments.contains("-v")
+    let verbose = CommandLine.arguments.contains("-v")
 
-let tcattr = UnsafeMutablePointer<termios>.allocate(capacity: 1)
-tcgetattr(FileHandle.standardInput.fileDescriptor, tcattr)
-let oldValue = tcattr.pointee.c_lflag
-atexit_b {
-    tcattr.pointee.c_lflag = oldValue
-    tcsetattr(FileHandle.standardInput.fileDescriptor, TCSAFLUSH, tcattr)
-    tcattr.deallocate()
-}
-tcattr.pointee.c_lflag &= ~UInt(ECHO | ICANON | ISIG)
-tcsetattr(FileHandle.standardInput.fileDescriptor, TCSAFLUSH, tcattr)
+        let tcattr = UnsafeMutablePointer<termios>.allocate(capacity: 1)
+        tcgetattr(FileHandle.standardInput.fileDescriptor, tcattr)
+        let oldValue = tcattr.pointee.c_lflag
+        atexit_b {
+            tcattr.pointee.c_lflag = oldValue
+            tcsetattr(FileHandle.standardInput.fileDescriptor, TCSAFLUSH, tcattr)
+            tcattr.deallocate()
+        }
+        tcattr.pointee.c_lflag &= ~UInt(ECHO | ICANON | ISIG)
+        tcsetattr(FileHandle.standardInput.fileDescriptor, TCSAFLUSH, tcattr)
 
-if (access("vdb.img", F_OK) != 0) {
-    if (fclose(fopen("vdb.img", "w")) != 0) {
-        perror("create vdb.img")
-        exit(1)
-    }
-    if (truncate("vdb.img", 10 * 1024 * 1024 * 1024) != 0) {
-        perror("resize vdb.img")
-        exit(1)
-    }
-}
+        if (access("vdb.img", F_OK) != 0) {
+            if (fclose(fopen("vdb.img", "w")) != 0) {
+                perror("create vdb.img")
+                exit(1)
+            }
+            if (truncate("vdb.img", 10 * 1024 * 1024 * 1024) != 0) {
+                perror("resize vdb.img")
+                exit(1)
+            }
+        }
 
-let config = VZVirtualMachineConfiguration()
-config.cpuCount = 2
-config.memorySize = 2 * 1024 * 1024 * 1024
+        let config = VZVirtualMachineConfiguration()
+        config.cpuCount = 2
+        config.memorySize = 2 * 1024 * 1024 * 1024
 
-do {
-    let vda = try VZDiskImageStorageDeviceAttachment(url: URL(fileURLWithPath: "vda.img"), readOnly: false)
-    config.storageDevices = [VZVirtioBlockDeviceConfiguration(attachment: vda)]
-} catch {
-    fatalError("Virtual Machine Primary Storage Error: \(error)")
-}
+        do {
+            let vda = try VZDiskImageStorageDeviceAttachment(url: URL(fileURLWithPath: "vda.img"), readOnly: false)
+            config.storageDevices = [VZVirtioBlockDeviceConfiguration(attachment: vda)]
+        } catch {
+            fatalError("Virtual Machine Primary Storage Error: \(error)")
+        }
 
-do {
-    let vdb = try VZDiskImageStorageDeviceAttachment(url: URL(fileURLWithPath: "vdb.img"), readOnly: false)
-    config.storageDevices += [VZVirtioBlockDeviceConfiguration(attachment: vdb)]
-} catch {
-    fatalError("Virtual Machine Secondary Storage Error: \(error)")
-}
+        do {
+            let vdb = try VZDiskImageStorageDeviceAttachment(url: URL(fileURLWithPath: "vdb.img"), readOnly: false)
+            config.storageDevices += [VZVirtioBlockDeviceConfiguration(attachment: vdb)]
+        } catch {
+            fatalError("Virtual Machine Secondary Storage Error: \(error)")
+        }
 
-let bootloader = VZLinuxBootLoader(kernelURL: URL(fileURLWithPath: "vmlinuz"))
-bootloader.commandLine = "console=hvc0 root=/dev/vda" + (verbose ? "" : " quiet")
-config.bootLoader = bootloader
+        let bootloader = VZLinuxBootLoader(kernelURL: URL(fileURLWithPath: "vmlinuz"))
+        bootloader.commandLine = "console=hvc0 root=/dev/vda" + (verbose ? "" : " quiet")
+        config.bootLoader = bootloader
 
-let fs0 = VZVirtioFileSystemDeviceConfiguration(tag: "fs0")
-fs0.share = VZMultipleDirectoryShare(directories: [
-    "home": VZSharedDirectory(url: FileManager.default.homeDirectoryForCurrentUser, readOnly: false),
-])
-config.directorySharingDevices = [fs0]
+        let fs0 = VZVirtioFileSystemDeviceConfiguration(tag: "fs0")
+        fs0.share = VZMultipleDirectoryShare(directories: [
+            "home": VZSharedDirectory(url: FileManager.default.homeDirectoryForCurrentUser, readOnly: false),
+        ])
+        config.directorySharingDevices = [fs0]
 
-let serial = VZVirtioConsoleDeviceSerialPortConfiguration()
-serial.attachment = VZFileHandleSerialPortAttachment(
-    fileHandleForReading: FileHandle.standardInput,
-    fileHandleForWriting: FileHandle.standardOutput
-)
-config.serialPorts = [serial]
+        let serial = VZVirtioConsoleDeviceSerialPortConfiguration()
+        serial.attachment = VZFileHandleSerialPortAttachment(
+            fileHandleForReading: FileHandle.standardInput,
+            fileHandleForWriting: FileHandle.standardOutput
+        )
+        config.serialPorts = [serial]
 
-config.entropyDevices = [VZVirtioEntropyDeviceConfiguration()]
+        config.entropyDevices = [VZVirtioEntropyDeviceConfiguration()]
 
-let network = VZVirtioNetworkDeviceConfiguration()
-if let macAddressString = try? String(contentsOfFile: ".virt.mac", encoding: .utf8),
-   let macAddress = VZMACAddress(string: macAddressString.trimmingCharacters(in: .whitespacesAndNewlines)) {
-    network.macAddress = macAddress
-} else {
-    let macAddressString = network.macAddress.string
-    NSLog("Using new MAC Address \(macAddressString)")
-    do {
-        try macAddressString.write(toFile: ".virt.mac", atomically: false, encoding: .utf8)
-    } catch {
-        fatalError("Virtual Machine Config Error: \(error)")
-    }
-}
-network.attachment = VZNATNetworkDeviceAttachment()
-config.networkDevices = [network]
+        let network = VZVirtioNetworkDeviceConfiguration()
+        if let macAddressString = try? String(contentsOfFile: ".virt.mac", encoding: .utf8),
+           let macAddress = VZMACAddress(string: macAddressString.trimmingCharacters(in: .whitespacesAndNewlines)) {
+            network.macAddress = macAddress
+        } else {
+            let macAddressString = network.macAddress.string
+            NSLog("Using new MAC Address \(macAddressString)")
+            do {
+                try macAddressString.write(toFile: ".virt.mac", atomically: false, encoding: .utf8)
+            } catch {
+                fatalError("Virtual Machine Config Error: \(error)")
+            }
+        }
+        network.attachment = VZNATNetworkDeviceAttachment()
+        config.networkDevices = [network]
 
-do {
-    try config.validate()
-} catch {
-    NSLog("Virtual Machine Config Error: \(error)")
-    exit(2)
-}
+        do {
+            try config.validate()
+        } catch {
+            NSLog("Virtual Machine Config Error: \(error)")
+            exit(2)
+        }
 
-let vm = VZVirtualMachine(configuration: config)
-let delegate = Delegate()
-vm.delegate = delegate
+        let vm = VZVirtualMachine(configuration: config)
+        let delegate = Delegate()
+        vm.delegate = delegate
 
-vm.start { result in
-    switch result {
-    case .success:
-        if verbose { NSLog("Virtual Machine Started") }
-    case let .failure(error):
-        fatalError("Virtual Machine Start Error: \(error)")
-    }
-}
+        vm.start { result in
+            switch result {
+            case .success:
+                if verbose { NSLog("Virtual Machine Started") }
+            case let .failure(error):
+                fatalError("Virtual Machine Start Error: \(error)")
+            }
+        }
 
 dispatchMain()
 
@@ -112,6 +112,7 @@ class Delegate : NSObject, VZVirtualMachineDelegate {
         if verbose { NSLog("Virtual Machine Stopped") }
         exit(0)
     }
+
     func virtualMachine(_ virtualMachine: VZVirtualMachine, didStopWithError error: Error) {
         fatalError("Virtual Machine Run Error: \(error)")
     }
