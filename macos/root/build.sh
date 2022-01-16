1ca5ad29bf5a06ae4d1bdbcde717ebc6e5fb7fbb
Alexander Pinske <alex@pinske.eu>
2022-01-16 20:06:43 +0000

    kernel 5.15.15

--- macos/root/build.sh
+++ macos/root/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -d build ]; then
     mkdir build
-    podman run -d --name build --net host -v $PWD/build:/root/build docker.io/ubuntu:20.10 sleep infinity
+    podman run -d --name build -v $PWD/build:/root/build docker.io/ubuntu:21.10 sleep infinity
     podman exec build sh -c 'apt update && apt install -y git && cd /root && git clone https://github.com/apinske/simpleos.git'
 else
     podman start build
@@ -10,5 +10,5 @@ ARCH=$(uname -m)
 if [ "$ARCH" = "aarch64" ]; then
     ARCH=arm64
 fi
-podman exec -e ARCH=$ARCH build sh -c 'cd /root/simpleos/macos && ./build.sh && cp vmlinuz ../../build/vmlinuz'
+podman exec -e ARCH=$ARCH -it build sh -c 'cd /root/simpleos/macos && ./build.sh && cp vmlinuz ../../build/vmlinuz'
 podman stop build
