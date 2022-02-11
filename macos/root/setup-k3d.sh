23a6edf64603c0b6b2dd0f173cb5cde31b0ca656
Alexander Pinske <alex@pinske.eu>
2022-02-11 19:30:02 +0000

    k3d

---
+++ macos/root/setup-k3d.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -f /usr/local/bin/k3d ]; then
+  VERSION=5.3.0
+  ARCH=arm64
+  if [ "$(uname -m)" = "x86_64" ]; then
+    ARCH=amd64
+  fi
+  wget -O /usr/local/bin/k3d https://github.com/rancher/k3d/releases/download/v$VERSION/k3d-linux-$ARCH
+  chmod +x /usr/local/bin/k3d
+fi
+
+k3d cluster create --network podman --k3s-arg --flannel-backend=none@server:*
--- macos/root/setup-podman.sh
+++ macos/root/setup-podman.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-if [ "$(apk info | grep podman)" = "podman" ]; then
-    echo "export CONTAINER_HOST=tcp://$(ip -o addr show | awk '{ print $4 }' | cut -d'/' -f1):58080"
-else
+if [ ! -f /usr/bin/podman ]; then
     mkdir /mnt/vdb/containers
     ln -s /mnt/vdb/containers /var/lib/containers
-    apk add podman
-    echo "::respawn:/usr/bin/podman system service --time=0 tcp:0.0.0.0:58080" >> /etc/inittab
+    apk add podman catatonit
+    echo "::respawn:/usr/bin/podman system service --time=0 unix:///var/run/docker.sock" >> /etc/inittab
     reboot
 fi
