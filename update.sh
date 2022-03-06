78b1f55a6c8f6d42395e49f5a2d4ee784a4eff9f
Alexander Pinske <alex@pinske.eu>
2022-03-06 13:47:00 +0100

    move

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
-    ssh srv2-ubuntu tar czf - -C simpleos_x86/macos vda.img vmlinuz | tar xzf -
+    ssh srv2-ubuntu tar czf - -C virt_x86 vda.img vmlinuz | tar xzf -
 else
-    ssh ubuntu tar czf - -C simpleos/macos vda.img vmlinuz | tar xzf -
+    ssh ubuntu tar czf - -C virt vda.img vmlinuz | tar xzf -
 fi
