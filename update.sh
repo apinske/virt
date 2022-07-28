e7d19f8096449d611c44c0fca07780e391a20628
Alexander Pinske <alex@pinske.eu>
2022-07-28 20:58:53 +0200

    cleanup

--- update.sh
+++ update.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-ARCH=$(uname -m)
-if [ "$ARCH" = "x86_64" ]; then
-    ssh srv2-ubuntu tar czf - -C virt_x86 vda.img vmlinuz | tar xzf -
-else
-    ssh ubuntu tar czf - -C virt vda.img vmlinuz | tar xzf -
-fi
+ssh ubuntu tar czf - -C repos/virt vda.img vmlinuz | tar xzf -
