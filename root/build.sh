c1ad2594c2ed94b130e35c6ea8fb808389fd544b
Alexander Pinske <alex@pinske.eu>
2022-07-28 09:47:17 +0200

    Update build.sh

--- root/build.sh
+++ root/build.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 if [ ! -d build ]; then
     mkdir build
-    podman run -d --name build -v $PWD/build:/root/build docker.io/ubuntu:21.10 sleep infinity
+    podman run -d --name build -v $PWD/build:/root/build docker.io/ubuntu:22.04 sleep infinity
     podman exec build sh -c 'apt update && apt install -y git && cd /root && git clone https://github.com/apinske/virt.git'
 else
     podman start build
 fi
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then
-    ARCH=arm64
-fi
-podman exec -e ARCH=$ARCH -it build sh -c 'cd /root/virt && ./build.sh && cp vmlinuz ../build/vmlinuz'
+podman exec -e -it build sh -c 'cd /root/virt && ./build.sh && cp vmlinuz ../build/vmlinuz'
 podman stop build
