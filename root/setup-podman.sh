c0c3fe012b1f13c6d73588eca0bf3700c88af631
Alexander Pinske <alex@pinske.eu>
2022-08-21 20:54:35 +0000

    tmp on vdb

--- root/setup-podman.sh
+++ root/setup-podman.sh
@@ -2,6 +2,9 @@
 if [ ! -f /usr/bin/podman ]; then
     mkdir /mnt/vdb/containers
     ln -s /mnt/vdb/containers /var/lib/containers
+    mkdir /mnt/vdb/tmp
+    rm -r /var/tmp
+    ln -s /mnt/vdb/tmp /var/tmp
     apk add podman
     echo "::respawn:/usr/bin/podman system service --time=0 unix:///var/run/docker.sock" >> /etc/inittab
     reboot
