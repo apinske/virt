b68bd02427ee749c6fc16f411d309bdcfd42be92
Alexander Pinske <alex@pinske.eu>
2023-03-24 13:20:00 +0000

    k3d 5.4.9

--- root/setup-k3d.sh
+++ root/setup-k3d.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /usr/local/bin/k3d ]; then
-  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.4.4/k3d-linux-arm64
+  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.4.9/k3d-linux-arm64
   chmod +x /usr/local/bin/k3d
 fi
 
