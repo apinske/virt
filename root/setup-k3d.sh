04570a5607a5a8608eca594d01cc05a7548f5db6
Alexander Pinske <alex@pinske.eu>
2023-05-17 21:36:52 +0000

    k3d 5.5.0

--- root/setup-k3d.sh
+++ root/setup-k3d.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 if [ ! -f /usr/local/bin/k3d ]; then
-  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.4.9/k3d-linux-arm64
+  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.5.0/k3d-linux-arm64
   chmod +x /usr/local/bin/k3d
 fi
 
 if [ ! -f /usr/local/bin/kubectl ]; then
-  wget -O /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.26.3/bin/linux/arm64/kubectl
+  # https://storage.googleapis.com/kubernetes-release/release/stable.txt
+  wget -O /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.27.1/bin/linux/arm64/kubectl
   chmod +x /usr/local/bin/kubectl
 fi
 
