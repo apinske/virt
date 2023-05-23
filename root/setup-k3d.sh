40d4d75e081d2a662a0ae37bf7d32ebee246940a
Alexander Pinske <alex@pinske.eu>
2023-05-23 20:43:30 +0000

    k3d 5.5.1

--- root/setup-k3d.sh
+++ root/setup-k3d.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /usr/local/bin/k3d ]; then
-  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.5.0/k3d-linux-arm64
+  wget -O /usr/local/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.5.1/k3d-linux-arm64
   chmod +x /usr/local/bin/k3d
 fi
 
@@ -12,4 +12,4 @@ if [ ! -f /usr/local/bin/kubectl ]; then
 fi
 
 podman network create k3d
-k3d cluster create --network k3d --k3s-arg --flannel-backend=host-gw@server:* --env IPTABLES_MODE=legacy@server:* --no-lb --api-port 127.0.0.1:6443
+k3d cluster create --network k3d --k3s-arg --flannel-backend=host-gw@server:* --env IPTABLES_MODE=legacy@server:* --api-port 127.0.0.1:6443 --port 127.0.0.1:80:80/tcp@server:* --port 127.0.0.1:443:443/tcp@server:*
