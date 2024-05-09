374c07863acbf05297625dbd689219c8995c4b68
Alexander Pinske <alex@pinske.eu>
2024-05-09 10:47:26 +0000

    build on Ubuntu 24.04 LTS

--- root/build.sh
+++ root/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -d build ]; then
     mkdir build
-    podman run -d --name build -v $PWD/build:/root/build docker.io/ubuntu:22.04 sleep infinity
+    podman run -d --name build -v $PWD/build:/root/build docker.io/ubuntu:24.04 sleep infinity
     podman exec build sh -c 'apt update && apt install -y git sudo wget xz-utils bc && cd /root && git clone https://github.com/apinske/virt.git'
 else
     podman start build
--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 if [ ! -f vda.img ]; then
-  wget -O- https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-arm64.tar.gz | tar xzf - jammy-server-cloudimg-arm64.img
-  mv jammy-server-cloudimg-arm64.img vda.img
+  wget -O- https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-arm64.tar.gz | tar xzf - noble-server-cloudimg-arm64.img
+  mv noble-server-cloudimg-arm64.img vda.img
   truncate -s 64G vda.img
 fi
 if [ ! -f initrd ]; then
-  wget -O initrd https://cloud-images.ubuntu.com/jammy/current/unpacked/jammy-server-cloudimg-arm64-initrd-generic
+  wget -O initrd https://cloud-images.ubuntu.com/noble/current/unpacked/noble-server-cloudimg-arm64-initrd-generic
 fi
 if [ ! -f vmlinuz ]; then
-  wget -O vmlinuz https://cloud-images.ubuntu.com/jammy/current/unpacked/jammy-server-cloudimg-arm64-vmlinuz-generic
+  wget -O vmlinuz https://cloud-images.ubuntu.com/noble/current/unpacked/noble-server-cloudimg-arm64-vmlinuz-generic
   gunzip -f -S '' vmlinuz
 fi
 if [ ! -f vdc.iso ]; then
