e342b139b85a53bfd79aecd7af5efdc9f619864b
Alexander Pinske <alex@pinske.eu>
2024-07-11 16:35:52 +0000

    alpine 3.20.1

--- buildImage.sh
+++ buildImage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -f alpine-minirootfs.tar.gz ]; then
-    wget -O alpine-minirootfs.tar.gz https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/aarch64/alpine-minirootfs-3.20.0-aarch64.tar.gz
+    wget -O alpine-minirootfs.tar.gz https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/aarch64/alpine-minirootfs-3.20.1-aarch64.tar.gz
 fi
 
 umount mnt
